34874969ea127612cd168bdd3441cf33cb7534ae
Zhen Jin <zhen.jin@wework.com>
2018-02-19 02:22:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,46 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def drew?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    all_x = win_combo.all? do |position|
+      board[position] == "X"
+    end
+    all_o = win_combo.all? do |position|
+      board[position] == "O"
+    end
+    if all_x && all_o
+      return TRUE
+    end
+  end
+  return false
+      
+end
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    all_x = win_combo.all? do |position|
+      board[position] == "X"
+    end
+    all_o = win_combo.all? do |position|
+      board[position] == "O"
+    end
+    if all_x && all_o
+      return false
+    elsif all_x || all_o
+      return win_combo
+    end
+  end
+  return false
+end
+
+def full?(board)
+  full = board.add? do |position|
+    board[position] != " "
+  end 
+  if drew?(board)
+    return true
+  end
+  return full
+end
