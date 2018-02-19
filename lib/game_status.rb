5b24f6c67bb120fd0e66adb4ba4c1904af64925f
Zhen Jin <zhen.jin@wework.com>
2018-02-19 02:25:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-def drew?(board)
+def draw?(board)
   WIN_COMBINATIONS.each do |win_combo|
     all_x = win_combo.all? do |position|
       board[position] == "X"
@@ -40,11 +40,11 @@ def won?(board)
 end
 
 def full?(board)
-  if drew?(board)
+  if draw?(board)
     return true
   end
   full = board.all? do |position|
-    board[position] != " "
+    board[position] <> " "
   end 
   
   return full
